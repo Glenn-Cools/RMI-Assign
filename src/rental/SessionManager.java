be637dd85fcf6c49b00f694d7eb027b15fadbf36
Glenn-Cools <glenn.cools1@student.kuleuven.be>
2016-11-09 13:33:23 +0100

    Patch 2: Loading Rentals

--- src/rental/SessionManager.java
+++ src/rental/SessionManager.java
@@ -6,58 +6,57 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SessionManager implements ISessionManager{
-	
-	public static void main(){
-		
+public class SessionManager implements ISessionManager {
+
+	public static void main(String args[]) {
+
 		System.setSecurityManager(null);
 		CarRentalCompany.main();
-		
-		try{
+
+		try {
 			SessionManager obj = new SessionManager();
 			ISessionManager stub = (ISessionManager) UnicastRemoteObject.exportObject(obj, 0);
-			
+
 			// bind to remote object's stub in the registry.
 			Registry registry = LocateRegistry.getRegistry();
 			registry.bind("ISessionManager", stub);
-			
+
 			System.err.println("Server rdy");
-		} catch ( Exception e) {
+		} catch (Exception e) {
 			System.err.println("Server exp: " + e.toString());
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
 	public static boolean isReady = false;
-	
-	private Map<String,Session> sessions = new HashMap<String,Session>();
-	
-	public Session getSession(String name){
-		
+
+	private Map<String, Session> sessions = new HashMap<String, Session>();
+
+	public Session getSession(String name) {
+
 		Session out = getSessions().get(name);
-        if (out == null) {
-            throw new IllegalArgumentException("Session doesn't exist!: " + name);
-        }
-        return out;
+		if (out == null) {
+			throw new IllegalArgumentException("Session doesn't exist!: " + name);
+		}
+		return out;
 	}
-	
-	private Map<String,Session> getSessions(){
+
+	private Map<String, Session> getSessions() {
 		return sessions;
 	}
-	
-	public synchronized ReservationSession createReservationSession(String name){
+
+	public synchronized ReservationSession createReservationSession(String name) {
 		sessions.put(name, new ReservationSession(name));
 		return (ReservationSession) getSession(name);
 	}
-	
-	public synchronized ManagerSession createManagerSession(String name,String companyName){
-		sessions.put(name, new ManagerSession(name,companyName));
+
+	public synchronized ManagerSession createManagerSession(String name, String companyName) {
+		sessions.put(name, new ManagerSession(name, companyName));
 		return (ManagerSession) getSession(name);
 	}
-	
-	public void endSession(String name){
+
+	public void endSession(String name) {
 		sessions.remove(name);
 	}
 
